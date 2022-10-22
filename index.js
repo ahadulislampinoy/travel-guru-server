a10365a446fb15b25997ae5b375bcb1bd73d5c77
ahadulislampinoy <ahadulislampinoy@gmail.com>
2022-10-23 02:39:30 +0600

    3rd commit

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
 const destinations = require("./data/destinations.json");
 const hotels = require("./data/hotels.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome To Travel Guru API");
